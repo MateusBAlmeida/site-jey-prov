f306a9adfe42469e4daf07bb4a19c6e7639955ca
Mateus Almeida <mateus.belchior.98@gmail.com>
2023-09-30 11:43:09 -0300

    Background style

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,24 @@ function App() {
   
 
   return (
-    <div className='min-h-screen flex bg-slate-800 justify-center'>
-      <div className="flex items-center gap-4">
-      <h1 className='font-extrabold text-cyan-50'>EM INVESTIGAÇÃO</h1>
+    <div className='min-h-screen flex bg-gradient-to-b from-[#01f8fd] via-[#0abebf] to-[#fa6970] justify-center items-center'>
+      <div className='justify-center '>
+      <div className="justify-center min-h-full flex p-6">
+      <h1 className='font-extrabold text-gray-800 text-6xl'>EM CONSTRUÇÃO</h1>
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 justify-center">
         
         <a href='https://www.instagram.com/jyngrey/'>
-          <InstagramIcon className='w-10 h-10 text-cyan-50' />
+          <InstagramIcon className='w-10 h-10 text-gray-800' />
         </a>
         <a href='https://twitter.com/jyngrey'>
-          <TwitterIcon className='w-10 h-10 mr-2' />
+          <TwitterIcon className='w-10 h-10 mr-2 text-gray-800' />
         </a>
         
           <span className="text-sm text-muted-foreground">
 
           </span>
+      </div>
       </div>
     </div>
   )
